0c48e490e579ecd204cd5337294d111338730fce
guilherme de araujo <guiam2909@gmail.com>
2026-08-10 17:16:19 -0300

    feat: fazendo tests com o modelo rafatorado e passando um filtro de smoothing na funcao treinar modelo

--- notebooks/model_pipeline.py
+++ notebooks/model_pipeline.py
@@ -1,6 +1,13 @@
 from sklearn.ensemble import AdaBoostRegressor
+from sklearn.tree import DecisionTreeRegressor
 
 def treinar_modelo_adaboost(X, y):
-    modelo = AdaBoostRegressor(random_state=42)
+    base_estimator = DecisionTreeRegressor(max_depth=6)
+    modelo = AdaBoostRegressor(
+        estimator=base_estimator, 
+        n_estimators=150, 
+        learning_rate=0.05, 
+        random_state=42
+    )
     modelo.fit(X, y)
     return modelo
